df55134ab75786a07e5f69aeb84b202c83cbc0e8
Emmanuel Ortiz <ortizemmanuel219@gmail.com>
2017-08-20 15:50:12 -0700

    Add video display

--- video/views.py
+++ video/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Video
 
 # Create your views here.
 
 def video_list(request):
- return render(request, 'video_list.html',{})
+ videos = Video.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+ return render(request, 'video_list.html',{'videos':videos})
+
