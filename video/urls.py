c67f7f3165958c56016dc95f41f35f0c9468e393
Emmanuel Ortiz <ortizemmanuel219@gmail.com>
2017-08-20 22:29:05 -0700

    Views for detail Boku no hero chapters

--- video/urls.py
+++ video/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.video_list),
+	url(r'^video/(?P<pk>[0-9]+)/$',views.video_detail),
     ]
--- video/views.py
+++ video/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Video
 
@@ -7,4 +7,8 @@ from .models import Video
 def video_list(request):
  videos = Video.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
  return render(request, 'video_list.html',{'videos':videos})
+
+def video_detail(request, pk):
+        videos = get_object_or_404(Video, pk=pk)
+        return render(request, 'video_detail.html', {'videos': videos})
 
